66ad5c6c7e2d16c4dd5c8daf7ec6080f78fe8b94
Abdul Hadi <abdul.hade@hotmail.com>
2021-02-15 16:08:24 +0000

    header done. Content still working in.

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+
+var toggleMenu = () => {
+  var menu = document.getElementById("social-media-menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+};
